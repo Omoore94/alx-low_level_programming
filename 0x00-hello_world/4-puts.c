8abd84067ecf71180410ba98bc26dec6fe34eeb8
Omoore94 <omoh.afekhume1@gmail.com>
2022-03-10 12:59:08 -0800

    This program prints out a string to the standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main -Prints out a string to the standard out.
+ * Return: 0 if function is successful.
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
