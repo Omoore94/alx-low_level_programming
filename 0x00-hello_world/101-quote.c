5caccf359db7dc6054ede7939442d44fdab3e587
Omoore94 <omoh.afekhume1@gmail.com>
2022-03-10 19:41:45 -0800

    This program prints a string flld by a new line to the stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This is the entry points
+ *
+ * Return - 0 if successful
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
