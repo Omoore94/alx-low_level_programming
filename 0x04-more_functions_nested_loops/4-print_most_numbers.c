0d761cfaa6d4689dfc168d421eb624ae43bf8dc2
Omoore94 <omoh.afekhume1@gmail.com>
2022-03-18 16:16:20 -0700

    a function that prints the numbers, from 0 to 9, Do not print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++) /* 48 to 57 is the ASCII number range for 0 to 9 */
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putcahr(10]; /* this i code in a new line */
+}
